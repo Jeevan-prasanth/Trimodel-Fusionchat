d8dc82cf3673f2b6bfa714eea29eebd66bc56013
Jeevanprasanth T <142791947+Jeevan-prasanth@users.noreply.github.com>
2024-03-23 18:21:10 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -22,70 +22,68 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 
 def get_pdf_text(pdf_docs):
-    text=""
-    for pdf in pdf_docs:
-        pdf_reader= PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text+= page.extract_text()
-    return  text
+  text=""
+  for pdf in pdf_docs:
+    pdf_reader= PdfReader(pdf)
+    for page in pdf_reader.pages:
+      text+= page.extract_text()
+  return text
 
 
 
 def get_text_chunks(text):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
-    chunks = text_splitter.split_text(text)
-    return chunks
+  text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+  chunks = text_splitter.split_text(text)
+  return chunks
 
 
 def get_vector_store(text_chunks):
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-    vector_store = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
-    if vector_store is None:
-        vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
-        vector_store.save_local("faiss_index")
-    return vector_store
+  embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+  # Clear existing store (optional)
+  # new_db.reset()
+  vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+  vector_store.save_local("faiss_index")
+  return vector_store
 
 
 
 def get_conversational_chain():
 
-    prompt_template =  """
-    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
-    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
-    Context:\n {context}?\n
-    Question: \n{question}\n
+  prompt_template =  """
+  Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+  provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+  Context:\n {context}?\n
+  Question: \n{question}\n
 
-    Answer:
-    """
+  Answer:
+  """
 
-    model = ChatGoogleGenerativeAI(model="gemini-pro",
-                             temperature=0.3)
+  model = ChatGoogleGenerativeAI(model="gemini-pro",
+                                 temperature=0.3)
 
-    prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
-    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+  prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
+  chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
 
-    return chain
+  return chain
 
 
 
 def user_input(user_question):
-    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
-    
-    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+  embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+   
+  new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
 
-    docs = new_db.similarity_search(user_question)
+  docs = new_db.similarity_search(user_question)
 
-    chain = get_conversational_chain()
+  chain = get_conversational_chain()
 
-    
-    response = chain(
-        {"input_documents":docs, "question": user_question}
-        , return_only_outputs=True)
+   
+  response = chain(
+      {"input_documents":docs, "question": user_question}
+      , return_only_outputs=True)
 
-    print(response)
-    st.write("Reply: ", response["output_text"])
-
-
+  print(response)
+  st.write("Reply: ", response["output_text"])
 
 
 def main():
